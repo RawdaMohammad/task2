bf63f30be45246be562ffa64ac2647130a018356
Rawda Mohammad <117312444+RawdaMohammad@users.noreply.github.com>
2024-05-18 11:19:01 +0300

    list_files.bat

--- list_files.bat
+++ list_files.bat
@@ -1,9 +1,3 @@
 @echo off
 
-:: This is a Batch script to execute the 'dir' command
-
-:: Print a message
-echo Executing 'dir' command to list files and directories:
-
-:: Execute the 'dir' command
 dir
